c8b7d45036b1470e687f661ea463e26c9eb6b8be
Seongho Bae <me@seonghobae.me>
2026-08-03 23:26:37 +0900

    style(planning): format asynchronous controller

--- apps/planning-service/src/main.ts
+++ apps/planning-service/src/main.ts
@@ -120,7 +120,10 @@ async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('v1');
   app.enableShutdownHooks();
-  await app.listen(Number(process.env.PLANNING_SERVICE_PORT ?? 4102), '0.0.0.0');
+  await app.listen(
+    Number(process.env.PLANNING_SERVICE_PORT ?? 4102),
+    '0.0.0.0',
+  );
 }
 
 void bootstrap();
