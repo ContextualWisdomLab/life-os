0f32c8bbf58ff894e87735a82c58a65e37f2bfc6
Seongho Bae <me@seonghobae.me>
2026-08-03 12:01:15 +0900

    feat(identity): enforce UUIDv4 identity identifiers in PostgreSQL

---
+++ apps/identity-service/migrations/0005_opaque_uuid_v4_identifiers.sql
@@ -0,0 +1,24 @@
+ALTER TABLE identity.users
+  ADD CONSTRAINT users_id_uuid_v4 CHECK (
+    id::text ~ '^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
+  );
+
+ALTER TABLE identity.external_identities
+  ADD CONSTRAINT external_identities_id_uuid_v4 CHECK (
+    id::text ~ '^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
+  );
+
+ALTER TABLE identity.workspaces
+  ADD CONSTRAINT workspaces_id_uuid_v4 CHECK (
+    id::text ~ '^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
+  );
+
+ALTER TABLE identity.sessions
+  ADD CONSTRAINT sessions_id_uuid_v4 CHECK (
+    id::text ~ '^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
+  );
+
+ALTER TABLE identity.oauth_transactions
+  ADD CONSTRAINT oauth_transactions_id_uuid_v4 CHECK (
+    id::text ~ '^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$'
+  );
